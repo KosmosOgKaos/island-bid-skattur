61cc4e331f70e0a656b004200e7ff7f8443aaa40
krillijuni <kristleifur@juni.is>
2025-05-05 14:10:09 +0000

    Remove GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { DemoController } from './demo/demo.controller';
 import { DemoService } from './demo/demo.service';
-import { DemoResolver } from './demo/demo.resolver';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      graphiql: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-    }),
-  ],
+  imports: [],
   controllers: [DemoController],
-  providers: [DemoService, DemoResolver],
+  providers: [DemoService],
 })
 export class AppModule {}
